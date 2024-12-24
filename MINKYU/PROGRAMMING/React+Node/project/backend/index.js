d76ef60f7f2e2c6889db47a82d7e1b2bec6c2773
minkyu1180 <minkyu1180@naver.com>
2024-12-24 20:45:33 +0900

    [Prog] React+Node : postman 활용(body-parser), nodemon 활용, 비밀 설정 정보 관리 postman을 활용하여 파싱하는 기술을 익혔다. express와 호환성이 좋은 body-parser을 설치하여 이를 활용 가능하다. nodemon을 설치하여 개발 환경에서 끊기지 않고 변경된 코드를 자동으로 추척하여 서버에 적용해 주는 방법을 알게 되었다. --save-dev를 통해 devDependencies에 추가하고, scripts 속성값에 backend - nodemon index.js를 추가하여 npm run backend로 개발할 것 같다. config 디렉터리의 dev.js, prod.js, key.js 폴더에서 각 환경에 따른 값들을 지정하고, .env 파일을 통해 중요한 정보를 관리할 수 있다. 참고: https://www.inflearn.com/course/%EB%94%B0%EB%9D%BC%ED%95%98%EB%A9%B0-%EB%B0%B0%EC%9A%B0%EB%8A%94-%EB%85%B8%EB%93%9C-%EB%A6%AC%EC%95%A1%ED%8A%B8-%EA%B8%B0%EB%B3%B8/dashboard

--- MINKYU/PROGRAMMING/React+Node/project/backend/index.js
+++ MINKYU/PROGRAMMING/React+Node/project/backend/index.js
@@ -1,24 +1,52 @@
 // index.js
 // 백엔드 서버의 시작 폴더
+// require
 require('dotenv').config();
 const express = require('express');
-const app = express();
-const port = 5000; // backend server port
-
-const url = process.env.MONGODB_URL;
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const config = require('./config/key');
+// other files
+const { User } = require('./models/User');
+
+// rest
+const app = express();
+const port = config.PORT;
+const mongoURI = config.mongoURI;
+app.use(bodyParser.urlencoded({ extended: true })); // URL-encoded 형식의 데이터 파싱
+app.use(bodyParser.json()); // JSON 형식의 데이터 파싱
+console.log('mongo URI : ', mongoURI);
 mongoose
-  .connect(url, {
-    // options
-    useNewUrlParser: true, // MongoDB의 새로운 연결 문자열 형식 지원
-    useUnifiedTopology: true, // MongoDB의 드라이버 연결 관리 방식 사용
-  })
+  .connect(mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch((error) => console.log(error));
 
 // api endpoint
 app.get('/', (req, res) => res.send('Hello World!'));
 
+// 사용자 등록 (post)
+app.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // 데이터 유효성 검사
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: '모든 필드를 입력하세요.' });
+  }
+
+  try {
+    // User 객체 생성
+    const user = new User({ name, email, password });
+    // DB에 저장
+    await user.save();
+    // 성공 응답 반환
+    return res.status(201).json({ message: '사용자 등록을 성공했습니다.' });
+  } catch (error) {
+    // 에러 처리
+    console.error(error);
+    console.log('hi');
+    res.status(500).json({ message: '서버 오류 입니다.' });
+  }
+});
 // server start at port
 app.listen(port, () =>
   console.log(`Backend Server is Running at http://localhost:${port}!`)
