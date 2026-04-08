675f12d389faa18578b3365e0546d24f5987a9b5
minkyu1180 <minkyu1180@naver.com>
2026-04-08 20:35:59 +0900

    [Prog] React: 05. State

---
+++ MINKYU/PROGRAMMING/FrontEnd/React/Basic/05. State/state-app/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ProductDetail from './components/ProductDetail';
+
+function App() {
+  return (
+    <div>
+      {/* <h2>hi</h2> */}
+      <ProductDetail />
+    </div>
+  );
+}
+
+export default App;
